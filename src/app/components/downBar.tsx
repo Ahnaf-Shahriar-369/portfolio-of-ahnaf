0e144bfe6c8064d31909258837dc315506b0cfb7
Nameless146 <mrnameless44@gmail.com>
2025-05-04 23:22:15 +0600

    Removes unused styles and optimizes layout
    Deletes unused global CSS and font imports to simplify styling.
    Replaces complex page structure with a minimal layout component.
    Removes legacy assets and references for a cleaner project structure.

---
+++ src/app/components/downBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function DownBar() {
+    return (
+    <>
+
+    <div className="container">
+
+    <div className="fixed bottom-0 left-0 right-0 mx-auto mb-[30px] w-fit bg-[#FFFFFF] text-white py-4 px-8 rounded-full shadow-lg flex justify-center items-center space-x-6 z-50">
+    
+    </div>
+
+    </div>
+
+    </>
+    )
+}
+
+export default DownBar
+
+
+
+
+
