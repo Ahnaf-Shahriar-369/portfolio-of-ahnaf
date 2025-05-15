1fcc0eadaf1493720bb6c6aa9dabb7c6024a8905
Nameless146 <mrnameless44@gmail.com>
2025-05-15 23:59:24 +0600

    fuck you 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <Head>
 
-        <link rel="icon" type="image/png" href="/favicon.png" /> 
+        <link rel="icon" href="/favicon.ico" />
 
       </Head>
         
